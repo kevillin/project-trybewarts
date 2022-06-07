f6f7164d2c5d0d595d6b2fb4d435ea85391fd383
Kevillin Santos <kevillin_santos@hotmail.com>
2022-06-07 18:59:59 -0300

    Requisito 17

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const buttonHeader = document.querySelector('#button-header');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const textArea = document.querySelector('textarea');
 
 buttonHeader.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     window.alert('Olá, Tryber!');
   } else window.alert('Email ou senha inválidos.');
 });
-
-
